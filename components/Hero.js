e336caf7237f66e926e893a58f54bd3d5f441a58
Hawyar <hawyarxoshnaw@gmail.com>
2020-11-30 12:46:31 -0500

    add firebase

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 import { up } from 'styled-breakpoints';
 import { Wrapper } from './atoms/Wrapper';
-import Image from 'next/image';
 
 const Hero = () => {
   return (
     <Wrapper>
       <Flex>
-        <h3 className='title'>
+        <Title>
           A tiny studio dedicated to creating wonderful things for the web
-        </h3>
+        </Title>
       </Flex>
     </Wrapper>
   );
@@ -17,38 +16,29 @@ const Hero = () => {
 
 const Flex = styled.div`
   display: flex;
-  /* background-color: ${({ theme }) => theme.colors.dark}; */
-
   ${up('sm')} {
     width: 65%;
     padding: 3rem 0;
   }
   flex-direction: row;
   justify-content: center;
-
-  .title {
-    font-size: 1.2rem;
-    font-weight: 700;
-
-    letter-spacing: -0.8px;
-    line-height: 1.1;
-    color: ${({ theme }) => theme.colors.dark};
-    ${up('sm')} {
-      letter-spacing: -2.5px;
-      font-size: 2rem;
-    }
-    ${up('md')} {
-      letter-spacing: -2.5px;
-      font-size: 4rem;
-    }
-  }
 `;
 
-const Info = styled.p`
-  padding-top: 1.5rem;
-  font-size: 1.5rem;
+const Title = styled.p`
+  font-size: 1.2rem;
+  font-weight: 700;
 
-  line-height: 1.2;
+  letter-spacing: -0.8px;
+  line-height: 1.1;
+  color: ${({ theme }) => theme.colors.dark};
+  ${up('sm')} {
+    letter-spacing: -2.5px;
+    font-size: 2rem;
+  }
+  ${up('md')} {
+    letter-spacing: -2.5px;
+    font-size: 4rem;
+  }
 `;
 
 export default Hero;
--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,20 @@ import { theme } from '../theme/theme';
 import { ReactQueryCacheProvider, QueryCache } from 'react-query';
 import { ReactQueryDevtools } from 'react-query-devtools';
 import { Reset } from 'styled-reset';
-
+import { AuthProvider } from '../lib/auth';
 const queryCache = new QueryCache();
 
 export default function App({ Component, pageProps }) {
   return (
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <ThemeProvider theme={theme}>
-        <Reset />
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ReactQueryCacheProvider>
+    <AuthProvider>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <ThemeProvider theme={theme}>
+          <Reset />
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ReactQueryCacheProvider>
+    </AuthProvider>
   );
 }
